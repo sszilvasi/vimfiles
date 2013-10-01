0d441359d05447b44ba15bc2e74bd6f2f147cdbb
Sandor Szilvasi <sandor.szilvasi@gmail.com>
2013-10-01 11:37:45 -0500

    Playing around with setup-vim.sh

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,18 +1,24 @@
 #/bin/bash
 
+# Usage: curl https://raw.github.com/sszilvasi/vimfiles/master/setup-vim.sh | bash
+
 
 # Check if ~/.vimrc exists
 # Create backup
 # Create new .vimrc (copy)
+
+if [ -d ~/.vim ]; then
+    mv ~/.vim ~/.vim.old
+fi
+
+git clone https://sszilvasi@github.com/sszilvasi/vimfiles.git ~/.vim
 
 # Vundle
 if [ -d ~/.vim/bundle/vundle ]; then
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
 
-# Check if ctags is installed
-# Install if necessary
-
-sudo apt-get install -y vim-gnome
+# Install
+sudo apt-get install -y vim vim-gnome
 sudo apt-get install -y ctags
 
